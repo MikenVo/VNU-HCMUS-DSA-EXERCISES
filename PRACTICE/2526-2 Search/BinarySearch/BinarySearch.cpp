9b1e53fa1fe8c9c974114797e6d0ab9af06d30fe
Curious Dude <mikenvo7k@outlook.com>
2026-01-29 15:19:35 +0700

    Finish Binary Search

---
+++ PRACTICE/2526-2 Search/BinarySearch/BinarySearch.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int binarySearchNearest(const vector<int>& a, int n, int x) {
+	int left = 0, right = n - 1, middle = (left + right) / 2;
+	while (abs(left - right) != 1) {
+		if (a[middle] == x) {
+			return a[middle];
+		}
+		else if (a[middle] < x) {
+			left = middle;
+			middle = (left + right) / 2;
+		}
+		else if (a[middle] > x) {
+			right = middle;
+			middle = (left + right) / 2;
+		}
+	}
+
+	int diff1 = abs(a[left] - x), diff2 = abs(a[right] - x);
+	if (diff1 > diff2) {
+		return a[right];
+	}
+	else if (diff1 < diff2) {
+		return a[left];
+	}
+	else {
+		if (a[left] > a[right]) {
+			return a[left];
+		}
+		else if (a[left] < a[right]) {
+			return a[right];
+		}
+	}
+
+	return -1;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, q;
+	cin >> n >> q;
+	if (n <= 0)
+		return 0;
+
+	vector<int> vals(n);
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> vals[i];
+	}
+
+	sort(vals.begin(), vals.end());
+
+	while (q--) {
+		int x;
+		cin >> x;
+		int nearest = binarySearchNearest(vals, n, x);
+		cout << nearest << endl;
+	}
+
+	return 0;
+}
