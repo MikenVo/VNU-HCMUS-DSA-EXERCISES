02dfb7d659fc9a2aa807d60464f12adabd29e2ea
Curious Dude <mikenvo7k@outlook.com>
2026-03-05 21:55:12 +0700

    Create MergeSort

--- PRACTICE/2526-2 Sort Advanced/MergeSort/MergeSort.cpp
+++ PRACTICE/2526-2 Sort Advanced/MergeSort/MergeSort.cpp
@@ -1,43 +1,84 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
+// void merge(int arr[], int left, int mid, int right, int num) {
+//     int n1 = mid - left + 1;
+//     int n2 = right - mid;
 
-    int* LEFT = new int[n1];
-    int* RIGHT = new int[n2];
+//     int* LEFT = new int[n1];
+//     int* RIGHT = new int[n2];
 
-    for (int i = 0; i < n1; i++) {
-        LEFT[i] = arr[left + i];
-    }
+//     for (int i = 0; i < n1; i++) {
+//         LEFT[i] = arr[left + i];
+//     }
 
-    for (int j = 0; j < n2; j++) {
-        RIGHT[j] = arr[mid + 1 + j];
-    }
+//     for (int j = 0; j < n2; j++) {
+//         RIGHT[j] = arr[mid + 1 + j];
+//     }
 
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (LEFT[i] <= RIGHT[j])
-            arr[k++] = LEFT[i++];
-        else
-            arr[k++] = RIGHT[j++];
-    }
-    while (i < n1)
-        arr[k++] = LEFT[i++];
-    while (j < n2)
-        arr[k++] = RIGHT[j++];
+//     int i = 0, j = 0, k = left;
+//     int firstindex = 0, secondindex = 0;
+//     while (i < n1 && j < n2) {
+//         if (LEFT[i] <= RIGHT[j]) {
+//             for (int i1 = 0; i1 < num; i1++) {
+//                 if (arr[i1] == LEFT[i]) {
+//                     firstindex = i1;
+//                     break;
+//                 }
+//             }
+//             arr[k++] = LEFT[i++];
+//         }
+//         else {
+//             for (int i2 = 0; i2 < num; i2++) {
+//                 if (arr[i2] == RIGHT[j]) {
+//                     secondindex = i2;
+//                     break;
+//                 }
+//             }
+//             arr[k++] = RIGHT[j++];
+//         }
 
-    delete[] LEFT;
-    delete[] RIGHT;
-}
+//         if (firstindex > secondindex) {
+//             cout << secondindex << " " << firstindex << endl;
+//         }
+//         else if (firstindex < secondindex) {
+//             cout << firstindex << " " << secondindex << endl;
+//         }
+//     }
 
-void mergeSort(int arr[], int left, int right) {
+//     while (i < n1) {
+//         arr[k++] = LEFT[i++];
+//     }
+
+//     while (j < n2) {
+//         arr[k++] = RIGHT[j++];
+//     }
+
+//     delete[] LEFT;
+//     delete[] RIGHT;
+// }
+
+// void mergeSort(int arr[], int left, int right, int num) {
+//     if (left < right) {
+//         int mid = left + (right - left) / 2;
+//         mergeSort(arr, left, mid, num);
+//         mergeSort(arr, mid + 1, right, num);
+//         merge(arr, left, mid, right, num);
+//     }
+// }
+
+void mergeSort(int arr[], int left, int right, int num) {
+    int mid = left + (right - left) / 2;
     if (left < right) {
-        int mid = (left + (right - left)) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        mergeSort(arr, left, mid, num);
+        mergeSort(arr, mid + 1, right, num);
+    }
+    else {
+        if (arr[left] > arr[right + 1]) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
     }
 }
 
@@ -49,7 +90,8 @@ int main() {
         cin >> arr[i];
     }
 
-    mergeSort(arr, 0, num - 1);
+    int left = 0, right = num - 1;
+    mergeSort(arr, left, right, num);
 
     for (int i = 0; i < num; i++) {
         cout << arr[i] << " ";
