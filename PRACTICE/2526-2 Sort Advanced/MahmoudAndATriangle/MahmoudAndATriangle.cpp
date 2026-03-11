4757586196c8aef7d03b234b43022191fff7c053
MikenVo <mikentechno7k@gmail.com>
2026-03-11 10:49:56 +0700

    Doing MahmoudAndATriangle

--- PRACTICE/2526-2 Sort Advanced/MahmoudAndATriangle/MahmoudAndATriangle.cpp
+++ PRACTICE/2526-2 Sort Advanced/MahmoudAndATriangle/MahmoudAndATriangle.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void ShellSort(int arr[], int n) {
-    int gap = n / 2;
-    
-    while (gap >= 1) {
-        for (int i = gap; i < n; i += gap) {
-            int j = i;
-            while (j >= gap && arr[j] < arr[j - gap]) {
-                int temp = arr[j];
-                arr[j] = arr[j - gap];
-                arr[j - gap] = temp;
-                j -= gap;
-            }
-        }
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-        gap /= 2;
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int start, int end) {
+    if (start < end) {
+        int mid = start + (end - start) / 2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, start, mid, end);
     }
 }
 
@@ -28,23 +45,11 @@ int main() {
         cin >> arr[i];
     }
 
-    ShellSort(arr, n);
+    mergeSort(arr, 0, n - 1);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
-                if (arr[i] + arr[j] > arr[k] && arr[i] + arr[k] > arr[j] && arr[j] + arr[k] > arr[i]) {
-                    should_accept = true;
-                    break;
-                }
-            }
-
-            if (should_accept) {
-                break;
-            }
-        }
-
-        if (should_accept) {
+    for (int i = 0 ; i < n - 2; i++) {
+        if (arr[i] + arr[i + 1] > arr[i + 2]) {
+            should_accept = true;
             break;
         }
     }
