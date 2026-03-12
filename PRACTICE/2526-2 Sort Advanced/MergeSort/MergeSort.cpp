96f9037faba88d7b1475fe000b0f9b400f7e7ed2
Curious Dude <mikenvo7k@outlook.com>
2026-03-12 21:20:07 +0700

    Doing MergeSort

--- PRACTICE/2526-2 Sort Advanced/MergeSort/MergeSort.cpp
+++ PRACTICE/2526-2 Sort Advanced/MergeSort/MergeSort.cpp
@@ -1,53 +1,47 @@
+// Read README.md in https://github.com/MikenVo/VNU-HCMUS-DSA-EXERCISES/blob/main/PRACTICE/2526-2%20Sort%20Advanced/MergeSort to know the problem
+
 #include <iostream>
 #include <vector>
 #include <utility>
 
 using namespace std;
 
-// TODO: auxiliary function here
+struct element {
+    int value;
+    int index;
+};
 
-void inPlaceMerge(vector<int> &a, int start, int mid, int end) {
-    int gap, firstindex, secondindex;
+void inPlaceMerge(vector<element> &a, int start, int mid, int end) {
+    int length = end - start + 1;
+    int gap = (length / 2) + (length % 2);
 
-    for (gap = mid + 1; gap >= 1; gap /= 2) {
-        for (int i = gap; i < end + 1; i++) {
-            int temp = a[i];
-            int j;
-            for (j = i, firstindex = j; j >= gap && a[j - gap] > temp; j -= gap) {
-                a[j] = a[j - gap];
-                secondindex = j - gap;
-            }
-            a[j] = temp;
+    while (gap > 0) {
+        for (int i = start; i + gap <= end; i++) {
+            int j = i + gap;
 
-            if (firstindex >= 0 && secondindex >= 0) {
-                if (firstindex > secondindex) {
-                    cout << secondindex << " " << firstindex << endl;
-                }
-                else if (firstindex < secondindex) {
-                    cout << firstindex << " " << secondindex << endl;
-                }
-            }
-            else {
-                continue;
+            if (a[i].value > a[j].value) {
+                element temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+
+                cout << i << " " << j << "\n";
             }
         }
+
+        if (gap == 1)
+            break;
+
+        gap = (gap / 2) + (gap % 2);
     }
-    // TODO: inPlaceMerge logic, swapping and printing swapped positions here
 }
 
-void mergeSort(vector<int> &a, int start, int end) {
-    int mid = start + (end - start) / 2;
-    while (start < end) {
-        if (end - start == 1 && mid >= 0) {
-            inPlaceMerge(a, start, mid, end);
-            
-        }
-        else {
-            mergeSort(a, start, mid);
-            mergeSort(a, mid + 1, end);
-        }
+void mergeSort(vector<element> &a, int start, int end) {
+    if (start < end) {
+        int mid = start + (end - start) / 2;
+        mergeSort(a, start, mid);
+        mergeSort(a, mid + 1, end);
+        inPlaceMerge(a, start, mid, end);
     }
-    // TODO: Merge sort with in-place merge calling here
 }
 
 int main() {
@@ -57,16 +51,13 @@ int main() {
     int n;
     if (!(cin >> n))
         return 0;
-    vector<int> a(n);
+    vector<element> a(n);
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
+        cin >> a[i].value;
+        a[i].index = i;
     }
 
     mergeSort(a, 0, n - 1);
-
-    // for (int i = 0; i < n; i++) {
-    //     cout << a[i] << " ";
-    // }
 
     return 0;
 }
