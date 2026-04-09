0753df14a53e427c4f1545bf03d19de88ef462ab
Curious Dude <mikenvo7k@outlook.com>
2026-04-09 21:10:40 +0700

    Doing EnemyIsWeak

---
+++ PRACTICE/2526-2 Trees/EnemyIsWeak/EnemyIsWeak.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void add(long long occurence[], long long arr[], long long current) {
+    for ()
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n; // The number of men in Roman army
+    cin >> n;
+
+    long long* arr = new long long[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    long long MAX_VALUE = 1000000000;
+    long long* occurence = new long long[MAX_VALUE]();
+
+
+
+    long long ans = 0;
+
+
+    // Add the occurence of the current element
+    // Check how many number greater/smaller than the current element
+    // Multiply the left and right
+
+    
+
+    cout << ans;
+
+    return 0;
+}
